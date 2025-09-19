63f1c2da78baa8eaf6ef12e8b03f8781b0d36557
RizkyRR27 <rizkyroza2005@gmail.com>
2025-09-19 14:01:52 +0700

    coolections

--- pertemuan4/bin/pertemuan4.dart
+++ pertemuan4/bin/pertemuan4.dart
@@ -1,3 +1,4 @@
+import  'dart:io';
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
 
 void main(List<String> arguments) {
@@ -16,13 +17,54 @@ void main(List<String> arguments) {
  //List<int> angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 //growable list
- var gl = [1, 2, 3];
-  gl.add(4);
-  gl.add(5);
-  gl.add(6);
-  gl.add(27);
+//  var gl = [1, 2, 3];
+//   gl.add(4);
+//   gl.add(5);
+//   gl.add(6);
+//   gl.add(27);
 
-  gl.remove(27);
-  print("data dalam gl: $gl");
+//   gl.remove(27);
+//   print("data dalam gl: $gl");
+
+//set
+// var setNilai1 = <int> {1, 2, 3, 4};
+// var setNilai2 = <int> {3, 3, 4, 5, 5, 5,};
+
+// print("set nilai 1: $setNilai1");
+// print("set nilai 2: $setNilai2");
+// print("gabungan set: ${setNilai1.union(setNilai2)}");
+// print("irisan set: ${setNilai1.intersection(setNilai2)}");
+// print("selisih set: ${setNilai1.difference(setNilai2)}");
+
+//collections
+// var setNilai1 = <int> {};
+// var setNilai2 = <int> {1,2,3,4,5};
+
+// print("jumlah data set nilai-1");
+// String? input = stdin.readLineSync(); 
+// int jumlah1 = (int.tryParse(input ?? '')) ?? 0;
+// for (var i = 0; i < jumlah1; i++) {
+//   print("masukkan nilai ke-${i+1}: ");
+//   String? nilai = stdin.readLineSync();
+//   int nilaiInt = (int.tryParse(nilai ?? '')) ?? 0;
+//   setNilai1.add(nilaiInt);
+// }
+// print("objects: data dalam set nilai-1: $setNilai1");
+
+var setNilai1 = <String> [];
+
+print("jumlah data set nilai-1");
+String? input = stdin.readLineSync();
+int jumlah1 = (int.tryParse(input ?? '')) ?? 0;
+for (var i = 0; i < jumlah1; i++) {
+  print("masukkan nilai ke-${i+1}: ");
+  String? nilai = stdin.readLineSync();
+  String nilaiInt = nilai ?? '';
+  setNilai1.add(nilaiInt);
+}
+var nilaiList = setNilai1.toList();
+print(nilaiList[1]);
+//print( setNilai1.elementAt(2)); //jika ingin diprint satu per satu
+//print( setNilai1.join(',')); //jika ingin diprint semua
 
 }
