9b967cc25fc97300f9bb0d93ddf4c6ee47a84ee8
RizkyRR27 <rizkyroza2005@gmail.com>
2025-09-19 11:21:35 +0700

    fixedlist

--- pertemuan4/bin/pertemuan4.dart
+++ pertemuan4/bin/pertemuan4.dart
@@ -1,5 +1,22 @@
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
 
 void main(List<String> arguments) {
-  print('Hello world: ${pertemuan4.calculate()}!');
+ //print('Hello world: ${pertemuan4.calculate()}!');
+
+ //fixed list
+ var fl = List<int>.filled(4, 0);
+
+  fl[0] = 1;  
+  fl[1] = 2;
+  fl[2] = 3;
+  fl[3] = 4;
+  fl[4] = 5;
+
+print("data dalam fl: $fl");
+ //List<int> angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+ 
+
+
+
 }
