117174dbbec37e7f0ec6611f9de6017f23f75ebb
RizkyRR27 <rizkyroza2005@gmail.com>
2025-09-20 06:18:36 +0700

    no1

---
+++ pertemuan4/bin/tugas4.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main(){
+//1. buat program untuk menyimpan nama mahasiswa mengguanakan  growable list, lalu tampilkan jumlah datanya
+List<String> namaMhs = [];
+namaMhs.add("Andi");
+namaMhs.add("Budi");  
+namaMhs.add("Caca");
+
+print("Jumlah data nama mahasiswa: ${namaMhs.length}");
+
+
+}
